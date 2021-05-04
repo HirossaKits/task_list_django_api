ec60c8c5a58e1ef54b6ab31fd906125a1f78c28e
kitsuka <kitsuka@example.com>
2021-05-04 17:07:42 +0900

    Register Views tp Urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
+from .view import TaskViewSet, CategoryViewSet, CreateUserView, LoginUserView, ProfileViewSet
 
 router = routers.DefaultRouter()
+router.register('category', CategoryViewSet)
+router.register('tasks', TaskViewSet)
+router.register('profile', ProfileViewSet)
 
 urlpatterns = [
-    path('',include(router.urls)),
+    path('create/', CreateUserView.as_view(), name='create'),
+    path('users/', ListUserView.as_view(), name='users'),
+    path('loginuser/',LoginUserView.as_view(), name='loginuser'),
+    path('', include(router.urls)),
 ]
+
